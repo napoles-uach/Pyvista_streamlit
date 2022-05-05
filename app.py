10d2255d12f89fbb340883a6f0757c2ef5440086
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-05-05 12:25:16 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import pyvista
+from pyvista import examples
+mesh = examples.load_uniform()
+pl = pyvista.Plotter(shape=(1,2))
+_ = pl.add_mesh(mesh, scalars='Spatial Point Data', show_edges=True)
+pl.subplot(0,1)
+_ = pl.add_mesh(mesh, scalars='Spatial Cell Data', show_edges=True)
+pl.export_html('pyvista.html')  # doctest:+SKIP
+
+option=st.sidebar.radio('Pyvista',('On','Off'))
+if option=='On':
+  HtmlFile = open("pyvista.html", 'r', encoding='utf-8')
+  source_code = HtmlFile.read() 
+  components.html(source_code, height = 500,width=500)
